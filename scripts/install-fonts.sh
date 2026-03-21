9458c877bb585c0b01b76539ed895fb585906419
Heron S. Silva <heron.code@outlook.com>
2026-03-21 19:15:05 -0300

    update install-fonts.sh adding p10k recommended meslo font

--- scripts/install-fonts.sh
+++ scripts/install-fonts.sh
@@ -11,8 +11,19 @@ for font in Iosevka IosevkaTerm IosevkaTermSlab JetBrainsMono; do
     curl -fL "https://github.com/ryanoasis/nerd-fonts/releases/latest/download/$font.tar.xz" | tar -xJ -C "$FONT_DIR"
 done
 
+echo "Installing p10k recommended fonts to $FONT_DIR"
+for font in "MesloLGS NF Regular.ttf" "MesloLGS NF Bold.ttf" "MesloLGS NF Italic.ttf" "MesloLGS NF Bold Italic.ttf"; do
+    echo "Downloading $font"
+
+    URL="https://github.com/romkatv/powerlevel10k-media/raw/refs/heads/master/$font"
+    ENCODED_FONT_NAME="${font// /%20}"
+
+    curl -L "https://github.com/romkatv/powerlevel10k-media/raw/master/$ENCODED_FONT_NAME" --output "$FONT_DIR/$font"
+done
+
 if [[ "$OSTYPE" != "darwin"* ]]; then
     fc-cache -fv
 fi
 
 echo "Fonts installed successfully!"
+
